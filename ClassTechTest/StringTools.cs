41190ac42541e271319f6736328c4db6516fcfb3
Danh <tqdanh@tma.com.vn>
2024-11-15 11:20:00 +0700

    implement FindFirstDuplicatedCharacter

--- ClassTechTest/StringTools.cs
+++ ClassTechTest/StringTools.cs
@@ -1,4 +1,6 @@
-﻿namespace ClassTechTest
+﻿using System.Collections.Generic;
+
+namespace ClassTechTest
 {
     public class StringTools
     {
@@ -6,6 +8,30 @@
 
         public string FindFirstDuplicatedCharacter(string input)
         {
+            if (string.IsNullOrEmpty(input))
+            {
+                return null;
+            }
+
+            var existedChars = new HashSet<char>();
+            foreach (var charValue in input)
+            {
+                var lowerChar = char.ToLower(charValue);
+                var upperChar = char.ToUpper(charValue);
+
+                if (existedChars.Contains(lowerChar))
+                {
+                    return lowerChar.ToString();
+                }
+
+                if (existedChars.Contains(upperChar))
+                {
+                    return upperChar.ToString();
+                }
+
+                existedChars.Add(charValue);
+            }
+
             return null;
         }
 
@@ -17,7 +43,7 @@
         {
             return true;
         }
-        
+
         public string FindLargestPalindrome(string input)
         {
             return null;
